634dea064c083c696364e59ff2fa98525c3956e2
Fahid Javid <fahidjavid@gmail.com>
2016-11-21 19:02:17 +0500

    Initial Commit.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Basic_Press
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'basic-press' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'basic-press' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'basic-press' ), 'basic-press', '<a href="https://www.fahidjavid.com" rel="designer">Fahid Javid</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
