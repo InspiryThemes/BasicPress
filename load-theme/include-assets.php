af0145c1a06b9c2835b30e2e446dde23c7c7c8bb
Fahid Javid <fahidjavid@gmail.com>
2016-12-27 13:17:03 +0500

    Moved assets inclusion to the assets file.

--- load-theme/include-assets.php
+++ load-theme/include-assets.php
@@ -1,6 +1,26 @@
 <?php
 
 /**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
+
+/**
  * Customizer Settings
  */
 require_once( get_template_directory() . '/inc/customizer/customizer.php' );
