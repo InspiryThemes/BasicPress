e1c4e43145e1c575aa2bd11aca8e9b93f9106a9f
Fahid Javid <fahidjavid@gmail.com>
2017-01-04 14:58:57 +0500

    Refined enqueuing scripts function.

--- functions.php
+++ functions.php
@@ -135,21 +135,56 @@ add_action( 'wp_enqueue_scripts', 'basic_press_styles' );
  * Enqueue scripts.
  */
 function basic_press_scripts() {
-    wp_enqueue_style( 'basic-press-style', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'basic-press-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    // navigation
+    wp_enqueue_script(
+        'basic-press-navigation',
+        get_template_directory_uri() . '/js/navigation.js',
+        array(),
+        '20151215',
+        true
+    );
 
-    wp_enqueue_script( 'basic-press-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    // skip link focus fix
+    wp_enqueue_script(
+        'basic-press-skip-link-focus-fix',
+        get_template_directory_uri() . '/js/skip-link-focus-fix.js',
+        array(),
+        '20151215',
+        true
+    );
 
-    wp_enqueue_script( 'tether', get_template_directory_uri() . '/js/tether.min.js', array( 'jquery' ), '4.0.0', true );
+    // tether
+    wp_enqueue_script(
+        'tether',
+        get_template_directory_uri() . '/js/tether.min.js',
+        array( 'jquery' ),
+        '4.0.0',
+        true
+    );
 
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'tether' ), '4.0.0', true );
+    // bootstrap
+    wp_enqueue_script(
+        'bootstrap',
+        get_template_directory_uri() . '/js/bootstrap.min.js',
+        array( 'tether' ),
+        '4.0.0',
+        true
+    );
 
+    // comment reply
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
 
-    wp_enqueue_script( 'basicpress', get_template_directory_uri() . '/js/basicpress.js', array( 'jquery' ), '1.0.0', true );
+    // custom
+    wp_enqueue_script(
+        'basicpress',
+        get_template_directory_uri() . '/js/basicpress.js',
+        array( 'jquery' ),
+        '1.0.0',
+        true
+    );
 }
 add_action( 'wp_enqueue_scripts', 'basic_press_scripts' );
 
