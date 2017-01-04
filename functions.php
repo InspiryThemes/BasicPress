e60e886daa9bcbca9f8b7f42d2187aac4b03ad06
Fahid Javid <fahidjavid@gmail.com>
2017-01-04 15:04:38 +0500

    Added wp editor styles.

--- functions.php
+++ functions.php
@@ -98,95 +98,113 @@ function basic_press_widgets_init() {
 }
 add_action( 'widgets_init', 'basic_press_widgets_init' );
 
-/**
- * Enqueue styles.
- */
-function basic_press_styles() {
+if( !function_exists( 'basicpress_add_editor_style' ) ) :
+    /**
+     * Add editor style
+     */
+    function basicpress_add_editor_style() {
+        add_editor_style( get_template_directory_uri() . '/css/editor-style.css' );
+    }
+    add_action( 'admin_init', 'basicpress_add_editor_style' );
+endif;
 
-    $template_directory_uri = get_template_directory_uri();
+if ( ! function_exists( 'basic_press_styles' ) ) {
+    /**
+     * Enqueue styles.
+     */
+    function basicpress_styles()
+    {
 
-    if ( ! is_admin() ) :
+        $template_directory_uri = get_template_directory_uri();
 
-        // bootstrap
-        wp_enqueue_style(
-            'bootstrap',
-            $template_directory_uri . '/css/bootstrap/bootstrap.css',
-            array(),
-            '4.0.0'
-        );
+        if (!is_admin()) :
 
-        // bootstrap
-        wp_enqueue_style(
-            'main',
-            $template_directory_uri . '/css/main.css',
-            array(),
-            '1.0.0'
-        );
+            // bootstrap
+            wp_enqueue_style(
+                'bootstrap',
+                $template_directory_uri . '/css/bootstrap/bootstrap.css',
+                array(),
+                '4.0.0'
+            );
 
-        wp_enqueue_style( 'basicpress-style', get_stylesheet_uri() );
+            // bootstrap
+            wp_enqueue_style(
+                'main',
+                $template_directory_uri . '/css/main.css',
+                array(),
+                '1.0.0'
+            );
 
-    endif;
+            wp_enqueue_style('basicpress-style', get_stylesheet_uri());
+
+        endif;
 
 
-}
-add_action( 'wp_enqueue_scripts', 'basic_press_styles' );
-
-/**
- * Enqueue scripts.
- */
-function basic_press_scripts() {
-
-    // navigation
-    wp_enqueue_script(
-        'basic-press-navigation',
-        get_template_directory_uri() . '/js/navigation.js',
-        array(),
-        '20151215',
-        true
-    );
-
-    // skip link focus fix
-    wp_enqueue_script(
-        'basic-press-skip-link-focus-fix',
-        get_template_directory_uri() . '/js/skip-link-focus-fix.js',
-        array(),
-        '20151215',
-        true
-    );
-
-    // tether
-    wp_enqueue_script(
-        'tether',
-        get_template_directory_uri() . '/js/tether.min.js',
-        array( 'jquery' ),
-        '4.0.0',
-        true
-    );
-
-    // bootstrap
-    wp_enqueue_script(
-        'bootstrap',
-        get_template_directory_uri() . '/js/bootstrap.min.js',
-        array( 'tether' ),
-        '4.0.0',
-        true
-    );
-
-    // comment reply
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
     }
 
-    // custom
-    wp_enqueue_script(
-        'basicpress',
-        get_template_directory_uri() . '/js/basicpress.js',
-        array( 'jquery' ),
-        '1.0.0',
-        true
-    );
+    add_action('wp_enqueue_scripts', 'basicpress_styles');
 }
-add_action( 'wp_enqueue_scripts', 'basic_press_scripts' );
+
+if ( ! function_exists( 'basicpress_scripts' ) ) {
+    /**
+     * Enqueue scripts.
+     */
+    function basicpress_scripts()
+    {
+
+        // navigation
+        wp_enqueue_script(
+            'basic-press-navigation',
+            get_template_directory_uri() . '/js/navigation.js',
+            array(),
+            '20151215',
+            true
+        );
+
+        // skip link focus fix
+        wp_enqueue_script(
+            'basic-press-skip-link-focus-fix',
+            get_template_directory_uri() . '/js/skip-link-focus-fix.js',
+            array(),
+            '20151215',
+            true
+        );
+
+        // tether
+        wp_enqueue_script(
+            'tether',
+            get_template_directory_uri() . '/js/tether.min.js',
+            array('jquery'),
+            '4.0.0',
+            true
+        );
+
+        // bootstrap
+        wp_enqueue_script(
+            'bootstrap',
+            get_template_directory_uri() . '/js/bootstrap.min.js',
+            array('tether'),
+            '4.0.0',
+            true
+        );
+
+        // comment reply
+        if (is_singular() && comments_open() && get_option('thread_comments')) {
+            wp_enqueue_script('comment-reply');
+        }
+
+        // custom
+        wp_enqueue_script(
+            'basicpress',
+            get_template_directory_uri() . '/js/basicpress.js',
+            array('jquery'),
+            '1.0.0',
+            true
+        );
+    }
+
+    add_action('wp_enqueue_scripts', 'basicpress_scripts');
+}
 
 /**
  * Load Theme
